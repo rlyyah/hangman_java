b195a8c2bbb02bf3d254cc7b77d3c7358a97a4c2
rlyyah <tmakowski1337@gmail.com>
2020-01-31 20:30:03 +0100

    Add abilty to enter 1 letter or whole password, show which letters has already been guessed and shows which of these are in the password

--- Hangman.java
+++ Hangman.java
@@ -1,18 +1,96 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
 
-public class hangman {
+public class Hangman {
 
     private String password;
     private String hint;
+    private Integer healthPoints = 10;
+    private Set<Character> lettersToGuess;
+    private Set<Character> lettersGuessed;
+    private String showToUser;
+    // private Set<Character> passwordSet;
+    // private Set<String> charSet;
     
-    public hangman(ArrayList<String> country, ArrayList<String> capital){
+    public Hangman(ArrayList<String> country, ArrayList<String> capital){
+
         Random random = new Random();
-        random.setSeed(1234567890);
+        int rand = 0;
         rand = random.nextInt(country.size());
+
+        System.out.println("Country size");
+        System.out.println(country.size());
+        System.out.println(rand);
         this.password = capital.get(rand);
         this.hint = country.get(rand);
+
+        lettersToGuess = new HashSet<>();
+        lettersGuessed = new HashSet<>();
+
+        for(int i =0; i<password.length();i++){
+            if(!Character.isWhitespace(password.charAt(i))){
+                lettersToGuess.add(password.toLowerCase().charAt(i));    
+            }
+            
+        }
+        System.out.println(lettersToGuess);
     }
+
+       
+    public String getInput(){
+        System.out.println("Enter a character or a whole word");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        System.out.println("Entered: " + input);
+        return input;
+    }
+
+    public Boolean isInPassword(String character){
+        Boolean letterInPassword = false;
+        
+        if(character.length()>1){
+            if(this.password.toLowerCase().compareTo(character.toLowerCase())==0){
+                letterInPassword = true;
+            }
+        } else {
+            lettersGuessed.add(character.toLowerCase().charAt(0));
+            for(char c: lettersToGuess){
+                if(c == character.toLowerCase().charAt(0)){
+                    letterInPassword = true;
+                }
+            }
+            
+        }
+
+        return letterInPassword;
+    }
+
+    public void letterFound(String ch){
+        if(ch.length()>1){
+            lettersToGuess.clear();
+        }else{
+            lettersToGuess.remove(ch.charAt(0));
+        }
+        
+    }
+
+    public void showUser(){
+        System.out.println();
+        for(int i=0;i<this.password.length();i++){
+            if(lettersGuessed.contains(this.password.toLowerCase().charAt(i))){
+                System.out.print(this.password.charAt(i));
+            }else{
+                System.out.print("_");
+            }
+            System.out.print(" ");
+            
+        }
+        System.out.println();
+    }
+
 
     public String getPassword() {
         return password;
@@ -21,6 +99,39 @@ public class hangman {
     public String getHint() {
         return hint;
     }
+
+    public Integer getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(Integer healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public Set<Character> getLettersToGuess() {
+        return lettersToGuess;
+    }
+
+    public void setLettersToGuess(Set<Character> lettersToGuess) {
+        this.lettersToGuess = lettersToGuess;
+    }
+
+    public Set<Character> getLettersGuessed() {
+        return lettersGuessed;
+    }
+
+    public void setLettersGuessed(Set<Character> lettersGuessed) {
+        this.lettersGuessed = lettersGuessed;
+    }
+
+    public String getShowToUser() {
+        return showToUser;
+    }
+
+    public void setShowToUser(String showToUser) {
+        this.showToUser = showToUser;
+    }
+    
 
 
 }
